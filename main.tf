ff85f8d75555ffc84a90178a551c57c53b392c2c
Daniel Iovescu <iovescu.dani@gmail.com>
2024-07-14 01:19:35 +0300

    Added input for the worker node module

--- main.tf
+++ main.tf
@@ -5,5 +5,6 @@ module "networking" {
 module "nodes" {
   source = "./modules/nodes"
   main_vpc_id = module.networking.main_vpc_id
+  main_vpc_cidr = module.networking.main_vpc_cidr
   public_subnet_id = module.networking.public_subnet_id
 }
--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -2,6 +2,10 @@ output "main_vpc_id" {
   value = aws_vpc.main_vpc.id
 }
 
+output "main_vpc_cidr" {
+  value = aws_vpc.main_vpc.cidr_block
+}
+
 output "public_subnet_id" {
   value = aws_subnet.public_subnet.id
 }
