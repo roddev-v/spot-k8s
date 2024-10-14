2b3af2dda25030b6dca6072c56603729196ae0f4
Daniel Iovescu <iovescu.dani@gmail.com>
2024-10-14 17:11:34 +0300

    Added user data for worker and master

--- modules/nodes/worker-nodes.tf
+++ modules/nodes/worker-nodes.tf
@@ -1,9 +1,18 @@
 resource "aws_instance" "worker_node" {
-  instance_type = var.worker_node_instance_type
-  ami           = var.ubuntu_24_04_lts_ami
-  subnet_id     = var.private_subnet_id
-  count         = var.worker_node_count
+  instance_type   = var.worker_node_instance_type
+  ami             = var.ubuntu_24_04_lts_ami
+  subnet_id       = var.private_subnet_id
+  count           = var.worker_node_count
   security_groups = [aws_security_group.spot_k8s_worker.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    NEW_HOSTNAME="spot-k8s-worker-${count.index}"
+    hostnamectl set-hostname "$NEW_HOSTNAME"
+    sed -i "s/127.0.1.1.*/127.0.1.1 $NEW_HOSTNAME/" /etc/hosts
+    echo "Hostname changed to $NEW_HOSTNAME"
+  EOF
+
 
   root_block_device {
     volume_size = var.worker_node_ebs_size
